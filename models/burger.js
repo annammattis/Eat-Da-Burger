a157df304c1809a2d20966da6b9e733abcf78e7b
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-01 13:48:03 -0400

    reworked controller

--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,7 @@
 
 
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js.js");
+var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
