69ba4bcfaf5360a722b1baddf4b937f70f9901e7
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-04 12:02:56 -0400

    changed route name

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -11,10 +11,10 @@ var burger = require("../models/burger.js");
 
 // get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
+  res.redirect("/burger");
 });
 
-router.get("/burgers", function(req, res) {
+router.get("/burger", function(req, res) {
   // express callback response by calling burger.selectAllBurger
   burger.all(function(burgerData) {
     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
@@ -23,7 +23,7 @@ router.get("/burgers", function(req, res) {
 });
 
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
+router.post("/burger/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
   burger.create(req.body.burger_name, function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
@@ -34,7 +34,7 @@ router.post("/burgers/create", function(req, res) {
 });
 
 // put route -> back to index
-router.put("/burgers/:id", function(req, res) {
+router.put("/burger/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
